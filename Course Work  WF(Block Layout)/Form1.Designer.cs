ccc0610d72cf5bccfc219df738ecfc04b5315233
Ильнар Мухутдинов <prizrak-im@inbox.ru>
2017-12-11 21:42:56 +0300

    Базовая разметка готова

--- Course Work  WF(Block Layout)/Form1.Designer.cs
+++ Course Work  WF(Block Layout)/Form1.Designer.cs
@@ -28,12 +28,156 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.enterNumOfElemInBlocks = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.enterNumOfBlocks = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.enterNumOfElem = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.apllyChanges = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BackColor = System.Drawing.Color.White;
+            this.splitContainer1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.apllyChanges);
+            this.splitContainer1.Panel1.Controls.Add(this.enterNumOfElemInBlocks);
+            this.splitContainer1.Panel1.Controls.Add(this.label3);
+            this.splitContainer1.Panel1.Controls.Add(this.enterNumOfBlocks);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.enterNumOfElem);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Cursor = System.Windows.Forms.Cursors.Default;
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dataGridView1);
+            this.splitContainer1.Size = new System.Drawing.Size(784, 561);
+            this.splitContainer1.SplitterDistance = 261;
+            this.splitContainer1.TabIndex = 0;
+            this.splitContainer1.SplitterMoved += new System.Windows.Forms.SplitterEventHandler(this.splitContainer1_SplitterMoved);
+            // 
+            // enterNumOfElemInBlocks
+            // 
+            this.enterNumOfElemInBlocks.Location = new System.Drawing.Point(24, 151);
+            this.enterNumOfElemInBlocks.Name = "enterNumOfElemInBlocks";
+            this.enterNumOfElemInBlocks.Size = new System.Drawing.Size(57, 20);
+            this.enterNumOfElemInBlocks.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(20, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(220, 38);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Введите количество элементов\r\nв каждом блок";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // enterNumOfBlocks
+            // 
+            this.enterNumOfBlocks.Location = new System.Drawing.Point(24, 87);
+            this.enterNumOfBlocks.Name = "enterNumOfBlocks";
+            this.enterNumOfBlocks.Size = new System.Drawing.Size(57, 20);
+            this.enterNumOfBlocks.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(20, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(195, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Введите количество блоков";
+            // 
+            // enterNumOfElem
+            // 
+            this.enterNumOfElem.Location = new System.Drawing.Point(24, 42);
+            this.enterNumOfElem.Name = "enterNumOfElem";
+            this.enterNumOfElem.Size = new System.Drawing.Size(57, 20);
+            this.enterNumOfElem.TabIndex = 1;
+            this.enterNumOfElem.TextChanged += new System.EventHandler(this.enterNumOfElem_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(20, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(220, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите количество элементов";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.dataGridView1.Location = new System.Drawing.Point(18, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(489, 537);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // apllyChanges
+            // 
+            this.apllyChanges.Location = new System.Drawing.Point(45, 200);
+            this.apllyChanges.Name = "apllyChanges";
+            this.apllyChanges.Size = new System.Drawing.Size(170, 40);
+            this.apllyChanges.TabIndex = 5;
+            this.apllyChanges.Text = "Принять";
+            this.apllyChanges.UseVisualStyleBackColor = true;
+            this.apllyChanges.Click += new System.EventHandler(this.apllyChanges_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox enterNumOfElem;
+        private System.Windows.Forms.TextBox enterNumOfBlocks;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox enterNumOfElemInBlocks;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button apllyChanges;
     }
 }
 
--- Course Work  WF(Block Layout)/Form1.cs
+++ Course Work  WF(Block Layout)/Form1.cs
@@ -15,5 +15,35 @@ namespace Course_Work__WF_Block_Layout_
         {
             InitializeComponent();
         }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void enterNumOfElem_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_SplitterMoved(object sender, SplitterEventArgs e)
+        {
+
+        }
+
+        private void apllyChanges_Click(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
